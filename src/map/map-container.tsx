d101f4d50b53071d0fce25e13eedf82dca547fdd
Rich McNeary <rlmcneary2@gmail.com>
2019-09-25 22:44:29 -0400

    Refactor. Add @pika/pack

--- src/map/map-container.tsx
+++ src/map/map-container.tsx
@@ -1,7 +1,7 @@
 import mapboxgl, { Map as MapboxMap } from "mapbox-gl";
 import React, { useEffect, useRef, useState } from "react";
 import { EventData } from "../types/event";
-import { BoundsOptions, CenterOptions, LngLat, LngLatBounds, ZoomOptions } from "../types/location";
+import { BoundsOptions, CenterOptions, FitBoundsOptions, LngLat, LngLatBounds, ZoomOptions } from "../types/location";
 import MapContextProvider from "./map-context";
 import MapData from "./map-data";
 
@@ -11,6 +11,7 @@ import MapData from "./map-data";
  */
 const MapContainer: React.FC<MapContainerProps> = ({
   accessToken,
+  animationOptions,
   as = "div",
   bounds: _bounds,
   center: _center = { center: [-68.2954881, 44.3420759] },
@@ -21,6 +22,7 @@ const MapContainer: React.FC<MapContainerProps> = ({
   maxBounds,
   maxZoom,
   minZoom,
+  motionType,
   onEvent,
   style = "mapbox://styles/mapbox/outdoors-v10",
   zoom: _zoom = { zoom: 9.5 }
@@ -41,9 +43,9 @@ const MapContainer: React.FC<MapContainerProps> = ({
 
     (mapboxgl.accessToken as any) = accessToken;
 
-    const { bounds, options: fitBoundsOptions } = _bounds || {};
-    const { center } = _center;
-    const { zoom } = _zoom;
+    const { bounds, fitBoundsOptions } = _bounds ? extractBounds(_bounds) : {} as any;
+    const { center } = extractCenter(_center);
+    const { zoom } = extractZoom(_zoom);
 
     const nextMap = new MapboxMap({
       ...defaultOptions,
@@ -85,8 +87,15 @@ const MapContainer: React.FC<MapContainerProps> = ({
    */
   useEffect(() => {
     if (map && _bounds) {
-      const { bounds, eventData, motionType, options } = _bounds;
-      map.fitBounds(bounds, { ...options, linear: motionType === "ease" }, eventData);
+      const { bounds, eventData } = extractBounds(_bounds);
+
+      map.fitBounds(
+        bounds,
+        animationOptions && motionType
+          ? { ...animationOptions, linear: motionType === "ease" }
+          : undefined,
+        eventData
+      );
     }
   }, [_bounds]);
 
@@ -171,11 +180,75 @@ const MapContainer: React.FC<MapContainerProps> = ({
 export default MapContainer;
 
 
+function extractBounds(
+  options: BoundsOptions | LngLatBounds
+): {
+  bounds: LngLatBounds;
+  eventData: {[key: string]: any} | undefined;
+  fitBoundsOptions: FitBoundsOptions | undefined
+} {
+  let bounds: LngLatBounds;
+  let eventData: {[key: string]: any} | undefined;
+  let fitBoundsOptions: FitBoundsOptions | undefined;
+  if (isBoundsOptions(options)) {
+    ({ bounds, eventData } = options);
+    const { maxZoom: boundsMaxZoom, padding } = options;
+    fitBoundsOptions = { maxZoom: boundsMaxZoom, padding };
+  } else {
+    bounds = options;
+  }
+
+  return {
+    bounds,
+    eventData,
+    fitBoundsOptions
+  };
+}
+
+function extractCenter(
+  options: CenterOptions | LngLat
+): { center: LngLat; eventData: {[key: string]: any} | undefined; } {
+  let center: LngLat;
+  let eventData: {[key: string]: any} | undefined;
+  if (isCenterOptions(options)) {
+    ({ center, eventData } = options);
+  } else {
+    center = options;
+  }
+
+  return {
+    center,
+    eventData
+  };
+}
+
+function extractZoom(
+  options: number | ZoomOptions | undefined
+): { zoom: number | undefined; eventData: {[key: string]: any} | undefined; } {
+  let zoom: number | undefined;
+  let eventData: {[key: string]: any} | undefined;
+  if (isZoomOptions(options)) {
+    ({ eventData, zoom } = (options as ZoomOptions));
+  } else if (options) {
+    zoom = options;
+  }
+
+  return {
+    eventData,
+    zoom
+  };
+
+}
+
 export interface MapContainerProps {
   /**
    * Sets the map's access token.
    */
   accessToken: string;
+  /**
+   * Options common to camera animation through bounds, center, or zoom
+   */
+  animationOptions?: AnimationOptions;
   /**
    * The name of an HTML element that hosts the map; defaults to div.
    */
@@ -183,14 +256,16 @@ export interface MapContainerProps {
   /**
    * Pans and zooms the map to contain its visible area within the specified
    * geographical bounds. Changing this value will cause the map position/zoom
-   * to change.
+   * to change. The appearance of the transition can be controlled through
+   * animationOptions and motionType.
    */
-  bounds?: BoundsOptions;
+  bounds?: LngLatBounds | BoundsOptions;
   /**
    * The geographical center point of the map. Changing this value will cause
-   * the map position to change.
+   * the map position to change. The appearance of the transition can be
+   * controlled through animationOptions and motionType.
    */
-  center?: CenterOptions;
+  center?: LngLat | CenterOptions;
   /**
    * A class name to set on the containing DIV element.
    */
@@ -217,6 +292,10 @@ export interface MapContainerProps {
    */
   minZoom?: number;
   /**
+   * How the camera moves when transitioning from one location to another through bounds, center, or zoom.
+   */
+  motionType?: "ease" | "fly" | "jump";
+  /**
    * Invoked when an event specified in "events" occurs.
    */
   onEvent: (data: EventData) => void;
@@ -226,7 +305,34 @@ export interface MapContainerProps {
    */
   style?: string;
   /**
-   * The zoom level of the map.
+   * The zoom level of the map. The appearance of the transition can be
+   * controlled through animationOptions and motionType.
    */
-  zoom?: ZoomOptions;
+  zoom?: number | ZoomOptions;
+}
+
+/**
+ * Options common to map movement methods that involve animation.
+ */
+interface AnimationOptions {
+  /**
+   * The animation's duration, measured in milliseconds.
+   */
+  duration?: number;
+  /**
+   * A function taking a time in the range 0..1 and returning a number where 0 is the initial state and 1 is the final state.
+   */
+  easing?: (input: number) => number;
+}
+
+function isBoundsOptions(options: LngLatBounds | BoundsOptions | undefined): options is BoundsOptions {
+  return !!options && "bounds" in options;
+}
+
+function isCenterOptions(options: LngLat | CenterOptions | undefined): options is CenterOptions {
+  return !!options && "center" in options;
+}
+
+function isZoomOptions(options: number | ZoomOptions | undefined): options is ZoomOptions {
+  return !!options && typeof options === "object";
 }
--- src/types/location.ts
+++ src/types/location.ts
@@ -1,6 +1,5 @@
-import { AnimationOptions, CameraOptions } from "mapbox-gl";
 
-interface BoundsOptions {
+export interface BoundsOptions extends FitBoundsOptions {
   /**
    * Center these bounds in the viewport and use the highest zoom level up to
    * and including maxZoom that fits them in the viewport.
@@ -11,27 +10,9 @@ interface BoundsOptions {
    * change.
    */
   eventData?: {[key: string]: any};
-  /**
-   * The type of motion to use during the transition.
-   */
-  motionType?: "ease" | "fly";
-  options?: BoundsTransitionOptions;
 }
 
-interface BoundsTransitionOptions extends AnimationOptions, CameraOptions {
-    /**
-     * Options for setting padding in pixels. All properties of this object must
-     * be non-negative integers.
-     */
-    padding?: { bottom: number; left: number; right: number; top: number; };
-    /**
-     * The maximum zoom level to allow when the map view transitions to the
-     * specified bounds.
-     */
-    maxZoom?: number;
-}
-
-interface CenterOptions {
+export interface CenterOptions {
   /**
    * The geographical center point of the map. Changing this value will cause
    * the map position to change.
@@ -42,12 +23,6 @@ interface CenterOptions {
    * change.
    */
   eventData?: {[key: string]: any};
-
-  /**
-   * The type of motion to use during the transition.
-   */
-  motionType?: "ease" | "fly" | "jump";
-  options: CameraOptions;
 }
 
 /**
@@ -55,29 +30,37 @@ interface CenterOptions {
  * signature the first element is the longitude and the second element is the
  * latitude.
  */
-type LngLat = [number, number] | { lat: number; lng: number; };
+export type LngLat = [number, number] | { lat: number; lng: number; };
 
 /**
  * Represent a geographic rectangle on a map. A tuple; the first element is the
  * south west corner, the second element is the north east corner.
  */
-type LngLatBounds = [LngLat, LngLat];
+export type LngLatBounds = [LngLat, LngLat];
 
-interface ZoomOptions {
+export interface FitBoundsOptions {
+  /**
+   * Options for setting padding in pixels. All properties of this object must
+   * be non-negative integers.
+   */
+  padding?: { bottom: number; left: number; right: number; top: number; };
+  /**
+   * The maximum zoom level to allow when the map view transitions to the
+   * specified bounds.
+   */
+  maxZoom?: number;
+}
+
+export interface ZoomOptions {
   /**
    * Additional properties to be added to event objects of events triggered by a
    * change.
    */
   eventData?: {[key: string]: any};
   /**
-   * The type of motion to use during the transition.
-   */
-  motionType?: "ease" | "fly" | "jump";
-  options?: AnimationOptions;
-  /**
    * The zoom level of the map.
    */
   zoom: number;
 }
 
-export { BoundsOptions, CenterOptions, LngLat, LngLatBounds, ZoomOptions };
+// export { BoundsOptions, CenterOptions, LngLat, LngLatBounds, ZoomOptions };
