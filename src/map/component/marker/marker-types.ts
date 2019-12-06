4b89b94d36907d092bb959f068b8b42550d41c41
rlmcneary2 <rich@bitovi.com>
2019-12-06 17:32:13 -0500

    Fix marker props.

--- src/map/component/marker/marker-types.ts
+++ src/map/component/marker/marker-types.ts
@@ -1,4 +1,4 @@
-import { PopupProps } from "../popup/popup-types";
+import { MarkerPopupProps } from "../popup/popup-types";
 import { LngLat, Point } from "../../../types/location";
 import { AnchorType } from "../../../types/ui";
 
@@ -71,7 +71,7 @@ export interface MarkerProps {
   /**
    * A Popup component with content for this marker.
    */
-  popup?: React.ReactElement<PopupProps>;
+  popup?: React.ReactElement<MarkerPopupProps>;
   /**
    * If true the popup will be shown.
    */
