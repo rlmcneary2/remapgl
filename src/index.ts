2561233db2b4d63382718439fc45c30765f6124d
rlmcneary2 <rich@bitovi.com>
2019-12-06 17:43:14 -0500

    Expose MarkerPopup.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import Layer from "./layer/mapboxgl/layer";
 import Map from "./map/map";
 import Marker from "./map/component/marker/marker";
+import MarkerPopup from "./map/component/marker/component/popup/popup";
 import Popup from "./map/component/popup/popup";
 import Attribution from "./user-interface/attribution";
 import Navigation from "./user-interface/navigation";
 
-export { Attribution, Layer, Map, Marker, Navigation, Popup };
+export { Attribution, Layer, Map, Marker, MarkerPopup, Navigation, Popup };
--- src/map/component/marker/component/popup/popup.tsx
+++ src/map/component/marker/component/popup/popup.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Popup from "../../popup/popup";
-import { MarkerPopupProps, PopupProps } from "../../popup/popup-types";
+import Popup from "../../../popup/popup";
+import { MarkerPopupProps, PopupProps } from "../../../popup/popup-types";
 
 export default function MarkerPopup(props: MarkerPopupProps): JSX.Element {
   return <Popup {...(props as PopupProps)} />;
